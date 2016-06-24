73e24fb067151362576fcc432219755989c7ba35
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-06-24 04:29:46 +0200

    initial setup

---
+++ wp-github-wiki.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: GitHub Wiki
+ * Plugin URI: https://www.usabilitydynamics.com
+ * Description: Get GitHub Wiki.
+ * Author: Usability Dynamics, Inc.
+ * Version: 1.9.0
+ * Text Domain: wp-github-wiki
+ * Author URI: https://www.usabilitydynamics.com
+ *
+ * Copyright 2012 - 2016 Usability Dynamics, Inc.  ( email : info@usabilitydynamics.com )
+ *
+ */
+
